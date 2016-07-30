614a11f5868568af7f816c0efa264c16eedb05bc
Jack Witbrock Finlay <jack@jackfinlay.com>
2016-07-30 20:11:22 +1200

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# jackfinlay.com
+ This is the repo for my personal website jackfinlay.com. Repositories named in the form <username>.github.io are automatically hosted as a website.
+ 
+ Updated 15/05/2016:
+
+ 	- Make navbar sticky.
+ 	- Updates for mobile friendlyness.
+ 	- Updated CV, added word format.
+
+ Updated 26/03/2016:
+
+ 	- Remove image.
+ 	- Update About text.
+
+ Updated 14/03/2016:
+
+ 	- Added footer.
+ 	- Added copyright notice.
+
+ Updated 13/03/2016:
+
+ 	- Updated CV.
+ 	- Made Nav responsive.
+ 	- Update meta tags; added keywords, description, and charset.
+
+ Updated 02/03/2016:
+
+ 	- make site actually responsive - added viewport tags.
+ 	- added .gitignore to make sure prepros files don't upload.
+ 	- Updated CV.
---
+++ assets/javascript/site.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+	
+	var hamburgerOpen = false;
+
+	var openMenu = function () {
+		
+		$("#nav").addClass("scroll-fix").animate({ height: "278px" },250);
+		$("#menu > li").css("display", "block");
+		
+		hamburgerOpen = true;
+	};
+
+	var closeMenu = function () {
+
+		if ($(window).scrollTop() <= ($(window).height() - 100)) {
+			$("#nav").animate({ height: "100px" }, 250, function (){
+
+				if ($(window).width() <= 1070) {
+					$('#menu > li:not(:first-child)').css("display", "none");
+				}
+
+				$("#nav").addClass("hide-scroll-fix");
+				$("#nav").removeClass("scroll-fix");
+				
+				setTimeout(function () {
+					$("#nav").removeClass("hide-scroll-fix");
+				}, 500);
+			});
+
+		} else if ($(window).width() <= 1070) {
+			$("#nav.scroll-fix").animate({ height: "100px" },250, function (){
+				$('#menu > li:not(:first-child)').css("display", "none");
+			});
+
+			
+		}
+
+		hamburgerOpen = false;
+	};
+
+	$("#hamburger").click(function() {
+		if (!hamburgerOpen) { 
+			openMenu();
+		} else {
+			closeMenu();
+		}
+
+	});
+
+	$(document).on('click', 'a', function(event){
+    	event.preventDefault();
+		
+		$('html, body').animate({
+        	scrollTop: $( $.attr(this, 'href') ).offset().top
+    		}, 400, function () {
+    			closeMenu()
+			}
+		);
+
+	});
+
+	$(window).scroll(function () { 
+		if ($(window).scrollTop() > ($(window).height() - 100)) {
+			$("#nav").addClass("scroll-fix");
+		} else if (!hamburgerOpen){
+			$("#nav").addClass("hide-scroll-fix");
+			$("#nav").removeClass("scroll-fix");
+			
+			setTimeout(function () {
+				$("#nav").removeClass("hide-scroll-fix");
+			}, 500);
+		}
+	  
+	});
+
+	$(window).resize(function () {
+		if ($(window).width() > 1070){
+			$("#menu > li").css("display", "inline-block");
+		} else {
+			$("#menu > li:not(:first-child)").css("display", "none");
+		}
+
+	});
+
+});
